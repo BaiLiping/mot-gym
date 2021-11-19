f2fc3c96710c2fad4f18d1c1c39a85c4916f90a7
RadarPerceptionProject <9875843+RadarPerceptionProject@user.noreply.gitee.com>
2021-11-19 16:22:27 +0800

    initial commit

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ This is the Multi_object Track Environment based on Openai Gym
 cd mot-gym
 pip install -e .
 ```
+
+# make MOT environment
+import gym
+env=gym.make('mot:MOT-v0')
--- mot/__init__.py
+++ mot/__init__.py
@@ -6,7 +6,6 @@ logger = logging.getLogger(__name__)
 register(
     id='MOT-v0',
     entry_point='mot.envs:MOTEnv',
-    timestep_limit=1000,
     reward_threshold=1.0,
     nondeterministic = True,
 )
